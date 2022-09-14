e417c16c381bed292981e825b2759a46f98fee6a
pollenjp <polleninjp@gmail.com>
2022-09-14 10:30:43 +0900

    refactoring

--- main.go
+++ main.go
@@ -10,35 +10,35 @@ func init() {
 	log.SetFlags(log.Lshortfile)
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	res := struct {
+		ContentType string `json:"content_type"`
+		Msg         string `json:"msg"`
+		A           string `json:"a"`
+		B           string `json:"b"`
+	}{
+		ContentType: r.Header.Get("Content-Type"),
+		Msg:         "hello",
+	}
+
+	if v := r.FormValue("a"); v != "" {
+		res.A = v
+	}
+
+	if v := r.FormValue("b"); v != "" {
+		res.B = v
+	}
+
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		log.Println("Error:", err)
+	}
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc(
-		"/", func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json; charset=utf-8")
-
-			res := struct {
-				ContentType string `json:"content_type"`
-				Msg         string `json:"msg"`
-				A           string `json:"a"`
-				B           string `json:"b"`
-			}{
-				ContentType: r.Header.Get("Content-Type"),
-				Msg:         "hello",
-			}
-
-			if v := r.FormValue("a"); v != "" {
-				res.A = v
-			}
-
-			if v := r.FormValue("b"); v != "" {
-				res.B = v
-			}
-
-			if err := json.NewEncoder(w).Encode(res); err != nil {
-				log.Println("Error:", err)
-			}
-		},
-	)
+	mux.HandleFunc("/", rootHandler)
 	s := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
